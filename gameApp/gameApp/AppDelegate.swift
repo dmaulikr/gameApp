c5fd04279c5d4a4cf51520ee20c08992badc6381
egirsova <liza@girsovs.com>
2015-09-21 13:08:34 -0500

    initial commit

---
+++ gameApp/gameApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  gameApp
+//
+//  Created by Liza Girsova on 9/21/15.
+//  Copyright (c) 2015 Girsova. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
